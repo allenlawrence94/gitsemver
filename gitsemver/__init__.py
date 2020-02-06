cf8670bc74b76bded54e38763eb02fb79b6c03fc
Allen Lawrence <allenlawrence94@gmail.com>
2020-02-05 18:23:09 -0800

    fix(): executable fixed for python 3.6 compatibility

--- gitsemver/__init__.py
+++ gitsemver/__init__.py
@@ -1,10 +1,10 @@
 from .index import semver
 from .config import Config
 from pathlib import Path
-from subprocess import run
+from subprocess import run, PIPE
 
 
 def main():
-    logs = run(['git', 'log'], capture_output=True).stdout.decode()
+    logs = run(['git', 'log'], stdout=PIPE, stderr=PIPE).stdout.decode()
     config = Config.from_yaml(Path.cwd() / 'gitsemver.yml')
     print(semver(logs, config))
